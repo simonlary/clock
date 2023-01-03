6578129c66e9f67ce8985b0d1af20e19e807b69f
simonlary <simonlary@gmail.com>
2023-01-02 23:54:54 -0500

    Add more than basic Chromecast support.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Clock
 const clock = document.getElementById("clock");
 const formatter = new Intl.DateTimeFormat(undefined, {
 	timeStyle: "medium",
@@ -6,3 +7,27 @@ const formatter = new Intl.DateTimeFormat(undefined, {
 setInterval(() => {
 	clock.textContent = formatter.format(new Date());
 }, 100);
+
+// Cast
+function loadScript(url) {
+	const script = document.createElement("script");
+	script.src = url;
+	document.body.appendChild(script);
+}
+
+// Check if we are on a Chromecast
+if (navigator.userAgent.includes("CrKey")) {
+	loadScript("//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js");
+} else {
+	window["__onGCastApiAvailable"] = (isAvailable) => {
+		console.log("__onGCastApiAvailable");
+		if (isAvailable) {
+			console.log("isAvailable");
+			cast.framework.CastContext.getInstance().setOptions({
+				receiverApplicationId: "F65B2EC2",
+				autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
+			});
+		}
+	};
+	loadScript("//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1");
+}
