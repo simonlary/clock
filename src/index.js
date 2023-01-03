4be75e9c1311b8da8fea39be04d473953ae7176b
simonlary <simonlary@gmail.com>
2023-01-03 00:11:31 -0500

    Remove logs and stop crash.

--- src/index.js
+++ src/index.js
@@ -9,31 +9,33 @@ setInterval(() => {
 }, 100);
 
 // Cast
-function loadScript(url) {
-	const script = document.createElement("script");
-	script.src = url;
-	document.body.appendChild(script);
-}
-function loadScriptContent(content) {
-	const script = document.createElement("script");
-	script.innerText = content;
-	document.body.appendChild(script);
+function loadScript(src) {
+	return new Promise((resolve, reject) => {
+		try {
+			const tag = document.createElement("script");
+			tag.async = true;
+			tag.src = src;
+			tag.addEventListener("load", () => resolve());
+			tag.addEventListener("error", () => reject());
+			document.head.appendChild(tag);
+		} catch (error) {
+			reject(error);
+		}
+	});
 }
 
 // Check if we are on a Chromecast
 if (navigator.userAgent.includes("CrKey")) {
-	loadScript("//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js");
-	loadScriptContent("cast.framework.CastReceiverContext.getInstance().start();");
+	await loadScript("//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js");
+	cast.framework.CastReceiverContext.getInstance().start();
 } else {
 	window["__onGCastApiAvailable"] = (isAvailable) => {
-		console.log("__onGCastApiAvailable");
 		if (isAvailable) {
-			console.log("isAvailable");
 			cast.framework.CastContext.getInstance().setOptions({
 				receiverApplicationId: "F65B2EC2",
 				autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
 			});
 		}
 	};
-	loadScript("//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1");
+	await loadScript("//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1");
 }
