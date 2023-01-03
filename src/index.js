8169f1e85d52325ba43b64a0567d263243ceba9a
simonlary <simonlary@gmail.com>
2023-01-02 23:18:42 -0500

    Give good style and favicon.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const clock = document.getElementById("clock");
-const formatter = new Intl.DateTimeFormat();
+const formatter = new Intl.DateTimeFormat(undefined, {
+	timeStyle: "medium",
+});
 
 setInterval(() => {
 	clock.textContent = formatter.format(new Date());
-}, 500);
+}, 100);
