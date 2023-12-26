8e8c2e3d47b6b98e74d4ba450680de3d708a282a
simonlary <simonlary@gmail.com>
2023-12-25 20:39:06 -0500

    Disable idle timeout and add cast button to sender.

--- index.js
+++ index.js
@@ -29,16 +29,48 @@ function initClock() {
 
 async function initReceiver() {
     await loadScript("//www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js");
-    cast.framework.CastReceiverContext.getInstance().start();
+    const options = new cast.framework.CastReceiverOptions();
+    options.disableIdleTimeout = true;
+    cast.framework.CastReceiverContext.getInstance().start(options);
 }
 
 async function initSender() {
     window["__onGCastApiAvailable"] = (isAvailable) => {
         if (isAvailable) {
+            // Setup the options
             cast.framework.CastContext.getInstance().setOptions({
                 receiverApplicationId: "F65B2EC2",
                 autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
             });
+
+            // Add the cast button
+            document.body.appendChild(document.createElement("google-cast-launcher"));
+
+            // Send a regular ping when connected so the app doesn't disconnect
+            // let interval = null;
+            // let requestId = 0;
+            // cast.framework.CastContext.getInstance().addEventListener(
+            //     cast.framework.CastContextEventType.SESSION_STATE_CHANGED, // TODO: Also check on start if there is already a session
+            //     (event) => {
+            //         console.log(event.sessionState);
+            //         if (event.sessionState === cast.framework.SessionState.SESSION_STARTED) {
+            //             interval = setInterval(() => {
+            //                 cast.framework.CastContext.getInstance()
+            //                     .getCurrentSession()
+            //                     .sendMessage("urn:x-cast:com.google.cast.media", {
+            //                         type: "GET_STATUS",
+            //                         requestId: requestId++,
+            //                     })
+            //                     .then(
+            //                         () => console.log("ping"),
+            //                         (error) => console.error(error)
+            //                     );
+            //             }, 1000);
+            //         } else if (event.sessionState === cast.framework.SessionState.SESSION_ENDING) {
+            //             clearInterval(interval);
+            //         }
+            //     }
+            // );
         }
     };
     await loadScript("//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1");
