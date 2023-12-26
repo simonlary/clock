0d2c8a4af5fdd0e5ccebaff14b38265c1b8bc073
simonlary <simonlary@gmail.com>
2023-12-25 20:51:48 -0500

    Cleanup comments.

--- index.js
+++ index.js
@@ -37,40 +37,11 @@ async function initReceiver() {
 async function initSender() {
     window["__onGCastApiAvailable"] = (isAvailable) => {
         if (isAvailable) {
-            // Setup the options
             cast.framework.CastContext.getInstance().setOptions({
                 receiverApplicationId: "F65B2EC2",
                 autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
             });
-
-            // Add the cast button
             document.body.appendChild(document.createElement("google-cast-launcher"));
-
-            // Send a regular ping when connected so the app doesn't disconnect
-            // let interval = null;
-            // let requestId = 0;
-            // cast.framework.CastContext.getInstance().addEventListener(
-            //     cast.framework.CastContextEventType.SESSION_STATE_CHANGED, // TODO: Also check on start if there is already a session
-            //     (event) => {
-            //         console.log(event.sessionState);
-            //         if (event.sessionState === cast.framework.SessionState.SESSION_STARTED) {
-            //             interval = setInterval(() => {
-            //                 cast.framework.CastContext.getInstance()
-            //                     .getCurrentSession()
-            //                     .sendMessage("urn:x-cast:com.google.cast.media", {
-            //                         type: "GET_STATUS",
-            //                         requestId: requestId++,
-            //                     })
-            //                     .then(
-            //                         () => console.log("ping"),
-            //                         (error) => console.error(error)
-            //                     );
-            //             }, 1000);
-            //         } else if (event.sessionState === cast.framework.SessionState.SESSION_ENDING) {
-            //             clearInterval(interval);
-            //         }
-            //     }
-            // );
         }
     };
     await loadScript("//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1");
